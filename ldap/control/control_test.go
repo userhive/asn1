65078f06ca23cd900a11d3fdd85440523b31b6d1
Kenneth Shaw <kenshaw@gmail.com>
2020-10-26 12:28:12 +0700

    Cleaning up control description code

--- ldap/control/control_test.go
+++ ldap/control/control_test.go
@@ -2,130 +2,55 @@ package control
 
 import (
 	"bytes"
-	"fmt"
 	"reflect"
-	"runtime"
 	"testing"
 
 	"github.com/userhive/asn1/ber"
 )
 
-func TestNewPaging(t *testing.T) {
+func TestEncodeDecode(t *testing.T) {
 	t.Parallel()
-	runTest(t, NewPaging(0))
-	runTest(t, NewPaging(100))
-}
-
-func TestNewManageDsaIT(t *testing.T) {
-	t.Parallel()
-	runTest(t, NewManageDsaIT(true))
-	runTest(t, NewManageDsaIT(false))
-}
-
-func TestNewMicrosoftChangeNotification(t *testing.T) {
-	t.Parallel()
-	runTest(t, NewMicrosoftChangeNotification())
-}
-
-func TestNewMicrosoftShowDeletedObjects(t *testing.T) {
-	t.Parallel()
-	runTest(t, NewMicrosoftShowDeletedObjects())
-}
-
-func TestNewString(t *testing.T) {
-	t.Parallel()
-	runTest(t, NewString("x", true, "y"))
-	runTest(t, NewString("x", true, ""))
-	runTest(t, NewString("x", false, "y"))
-	runTest(t, NewString("x", false, ""))
-}
-
-func runTest(t *testing.T, control Control) {
-	header := ""
-	if callerpc, _, line, ok := runtime.Caller(1); ok {
-		if caller := runtime.FuncForPC(callerpc); caller != nil {
-			header = fmt.Sprintf("%s:%d: ", caller.Name(), line)
+	tests := []Control{
+		NewManageDsaIT(false),
+		NewManageDsaIT(true),
+		NewMicrosoftChangeNotification(),
+		NewMicrosoftShowDeletedObjects(),
+		NewPaging(0),
+		NewPaging(100),
+		NewString("x", false, ""),
+		NewString("x", false, "y"),
+		NewString("x", true, ""),
+		NewString("x", true, "y"),
+	}
+	for i, test := range tests {
+		p := test.Encode()
+		encodedBytes := p.Bytes()
+		// Decode directly from the encoded packet (ensures Value is correct)
+		fromPacket, err := Decode(p)
+		if err != nil {
+			t.Errorf("test %d decoding encoded bytes control failed: %s", i, err)
 		}
-	}
-	p := control.Encode()
-	encodedBytes := p.Bytes()
-	// Decode directly from the encoded packet (ensures Value is correct)
-	fromPacket, err := Decode(p)
-	if err != nil {
-		t.Errorf("%sdecoding encoded bytes control failed: %s", header, err)
-	}
-	if !bytes.Equal(encodedBytes, fromPacket.Encode().Bytes()) {
-		t.Errorf("%sround-trip from encoded packet failed", header)
-	}
-	if reflect.TypeOf(control) != reflect.TypeOf(fromPacket) {
-		t.Errorf("%sgot different type decoding from encoded packet: %T vs %T", header, fromPacket, control)
-	}
-	// Decode from the wire bytes (ensures ber-encoding is correct)
-	dec, err := ber.ParseBytes(encodedBytes)
-	if err != nil {
-		t.Errorf("%sdecoding encoded bytes failed: %s", header, err)
-	}
-	fromBytes, err := Decode(dec)
-	if err != nil {
-		t.Errorf("%sdecoding control failed: %s", header, err)
-	}
-	if !bytes.Equal(encodedBytes, fromBytes.Encode().Bytes()) {
-		t.Errorf("%sround-trip from encoded bytes failed", header)
-	}
-	if reflect.TypeOf(control) != reflect.TypeOf(fromPacket) {
-		t.Errorf("%sgot different type decoding from encoded bytes: %T vs %T", header, fromBytes, control)
-	}
-}
-
-func TestDescribeControlManageDsaIT(t *testing.T) {
-	t.Parallel()
-	runAddDescriptions(t, NewManageDsaIT(false), "Control Type ("+ControlManageDsaIT.String()+")")
-	runAddDescriptions(t, NewManageDsaIT(true), "Control Type ("+ControlManageDsaIT.String()+")", "Criticality")
-}
-
-func TestDescribePaging(t *testing.T) {
-	t.Parallel()
-	runAddDescriptions(t, NewPaging(100), "Control Type ("+ControlPaging.String()+")", "Control Value ("+ControlPaging.String()+")")
-	runAddDescriptions(t, NewPaging(0), "Control Type ("+ControlPaging.String()+")", "Control Value ("+ControlPaging.String()+")")
-}
-
-func TestDescribeControlMicrosoftChangeNotification(t *testing.T) {
-	t.Parallel()
-	runAddDescriptions(t, NewMicrosoftChangeNotification(), "Control Type ("+ControlMicrosoftChangeNotification.String()+")")
-}
-
-func TestDescribeControlMicrosoftShowDeleted(t *testing.T) {
-	t.Parallel()
-	runAddDescriptions(t, NewMicrosoftShowDeletedObjects(), "Control Type ("+ControlMicrosoftShowDeletedObjects.String()+")")
-}
-
-func TestDescribeControlString(t *testing.T) {
-	t.Parallel()
-	runAddDescriptions(t, NewString("x", true, "y"), "Control Type (x)", "Criticality", "Control Value")
-	runAddDescriptions(t, NewString("x", true, ""), "Control Type (x)", "Criticality")
-	runAddDescriptions(t, NewString("x", false, "y"), "Control Type (x)", "Control Value")
-	runAddDescriptions(t, NewString("x", false, ""), "Control Type (x)")
-}
-
-func runAddDescriptions(t *testing.T, control Control, childDescriptions ...string) {
-	header := ""
-	if callerpc, _, line, ok := runtime.Caller(1); ok {
-		if caller := runtime.FuncForPC(callerpc); caller != nil {
-			header = fmt.Sprintf("%s:%d: ", caller.Name(), line)
+		if !bytes.Equal(encodedBytes, fromPacket.Encode().Bytes()) {
+			t.Errorf("test %d round-trip from encoded packet failed", i)
 		}
-	}
-	encodedControls := Encode(control)
-	AddDescriptions(encodedControls)
-	p := encodedControls.Children[0]
-	if len(p.Children) != len(childDescriptions) {
-		t.Errorf("%sinvalid number of children: %d != %d", header, len(p.Children), len(childDescriptions))
-	}
-	for i, desc := range childDescriptions {
-		/*if encodedPacket.Children[i].Desc != desc {
-			t.Errorf("%sdescription not as expected: %s != %s", header, encodedPacket.Children[i].Desc, desc)
+		if reflect.TypeOf(test) != reflect.TypeOf(fromPacket) {
+			t.Errorf("test %d got different type decoding from encoded packet: %T vs %T", i, fromPacket, test)
 		}
-		*/
-		i, desc = i, desc
+		// Decode from the wire bytes (ensures ber-encoding is correct)
+		dec, err := ber.ParseBytes(encodedBytes)
+		if err != nil {
+			t.Errorf("test %d decoding encoded bytes failed: %s", i, err)
+		}
+		fromBytes, err := Decode(dec)
+		if err != nil {
+			t.Errorf("test %d decoding control failed: %s", i, err)
+		}
+		if !bytes.Equal(encodedBytes, fromBytes.Encode().Bytes()) {
+			t.Errorf("test %d round-trip from encoded bytes failed", i)
+		}
+		if reflect.TypeOf(test) != reflect.TypeOf(fromPacket) {
+			t.Errorf("test %d got different type decoding from encoded bytes: %T vs %T", i, fromBytes, test)
+		}
 	}
 }
 
@@ -181,53 +106,14 @@ func TestDecode(t *testing.T) {
 			exp: &BeheraPasswordPolicy{Expire: -1, Grace: -1, Error: 8, ErrorString: "New password is in list of old passwords"}, err: false,
 		},
 	}
-	for i := range tests {
-		err := AddDescriptions(tests[i].args.packet)
-		if err != nil {
-			t.Fatal(err)
+	for i, test := range tests {
+		got, err := Decode(tests[i].args.packet.Children[0])
+		switch {
+		case err == nil && test.err:
+			t.Errorf("test %d Decode error = %v, wantErr %t", i, err, test.err)
+		case err == nil && !reflect.DeepEqual(got, test.exp):
+			t.Errorf("test %d Decode got = %v, want %v", i, got, test.exp)
 		}
-		tests[i].args.packet = tests[i].args.packet.Children[0]
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			got, err := Decode(test.args.packet)
-			if (err != nil) != test.err {
-				t.Errorf("DecodeControl() error = %v, wantErr %v", err, test.err)
-				return
-			}
-			if !reflect.DeepEqual(got, test.exp) {
-				t.Errorf("DecodeControl() got = %v, want %v", got, test.exp)
-			}
-		})
-	}
-}
-
-func TestAddDescriptions(t *testing.T) {
-	type args struct {
-		packet *ber.Packet
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{name: "timeBeforeExpiration", args: args{packet: decodePacket([]byte{0xa0, 0x29, 0x30, 0x27, 0x4, 0x19, 0x31, 0x2e, 0x33, 0x2e, 0x36, 0x2e, 0x31, 0x2e, 0x34, 0x2e, 0x31, 0x2e, 0x34, 0x32, 0x2e, 0x32, 0x2e, 0x32, 0x37, 0x2e, 0x38, 0x2e, 0x35, 0x2e, 0x31, 0x4, 0xa, 0x30, 0x8, 0xa0, 0x6, 0x80, 0x4, 0x7f, 0xff, 0xf6, 0x5c})}, wantErr: false},
-		{name: "graceAuthNsRemaining", args: args{packet: decodePacket([]byte{0xa0, 0x26, 0x30, 0x24, 0x4, 0x19, 0x31, 0x2e, 0x33, 0x2e, 0x36, 0x2e, 0x31, 0x2e, 0x34, 0x2e, 0x31, 0x2e, 0x34, 0x32, 0x2e, 0x32, 0x2e, 0x32, 0x37, 0x2e, 0x38, 0x2e, 0x35, 0x2e, 0x31, 0x4, 0x7, 0x30, 0x5, 0xa0, 0x3, 0x81, 0x1, 0x11})}, wantErr: false},
-		{name: "passwordExpired", args: args{packet: decodePacket([]byte{0xa0, 0x24, 0x30, 0x22, 0x4, 0x19, 0x31, 0x2e, 0x33, 0x2e, 0x36, 0x2e, 0x31, 0x2e, 0x34, 0x2e, 0x31, 0x2e, 0x34, 0x32, 0x2e, 0x32, 0x2e, 0x32, 0x37, 0x2e, 0x38, 0x2e, 0x35, 0x2e, 0x31, 0x4, 0x5, 0x30, 0x3, 0x81, 0x1, 0x0})}, wantErr: false},
-		{name: "accountLocked", args: args{packet: decodePacket([]byte{0xa0, 0x24, 0x30, 0x22, 0x4, 0x19, 0x31, 0x2e, 0x33, 0x2e, 0x36, 0x2e, 0x31, 0x2e, 0x34, 0x2e, 0x31, 0x2e, 0x34, 0x32, 0x2e, 0x32, 0x2e, 0x32, 0x37, 0x2e, 0x38, 0x2e, 0x35, 0x2e, 0x31, 0x4, 0x5, 0x30, 0x3, 0x81, 0x1, 0x1})}, wantErr: false},
-		{name: "passwordModNotAllowed", args: args{packet: decodePacket([]byte{0xa0, 0x24, 0x30, 0x22, 0x4, 0x19, 0x31, 0x2e, 0x33, 0x2e, 0x36, 0x2e, 0x31, 0x2e, 0x34, 0x2e, 0x31, 0x2e, 0x34, 0x32, 0x2e, 0x32, 0x2e, 0x32, 0x37, 0x2e, 0x38, 0x2e, 0x35, 0x2e, 0x31, 0x4, 0x5, 0x30, 0x3, 0x81, 0x1, 0x3})}, wantErr: false},
-		{name: "mustSupplyOldPassword", args: args{packet: decodePacket([]byte{0xa0, 0x24, 0x30, 0x22, 0x4, 0x19, 0x31, 0x2e, 0x33, 0x2e, 0x36, 0x2e, 0x31, 0x2e, 0x34, 0x2e, 0x31, 0x2e, 0x34, 0x32, 0x2e, 0x32, 0x2e, 0x32, 0x37, 0x2e, 0x38, 0x2e, 0x35, 0x2e, 0x31, 0x4, 0x5, 0x30, 0x3, 0x81, 0x1, 0x4})}, wantErr: false},
-		{name: "insufficientPasswordQuality", args: args{packet: decodePacket([]byte{0xa0, 0x24, 0x30, 0x22, 0x4, 0x19, 0x31, 0x2e, 0x33, 0x2e, 0x36, 0x2e, 0x31, 0x2e, 0x34, 0x2e, 0x31, 0x2e, 0x34, 0x32, 0x2e, 0x32, 0x2e, 0x32, 0x37, 0x2e, 0x38, 0x2e, 0x35, 0x2e, 0x31, 0x4, 0x5, 0x30, 0x3, 0x81, 0x1, 0x5})}, wantErr: false},
-		{name: "passwordTooShort", args: args{packet: decodePacket([]byte{0xa0, 0x24, 0x30, 0x22, 0x4, 0x19, 0x31, 0x2e, 0x33, 0x2e, 0x36, 0x2e, 0x31, 0x2e, 0x34, 0x2e, 0x31, 0x2e, 0x34, 0x32, 0x2e, 0x32, 0x2e, 0x32, 0x37, 0x2e, 0x38, 0x2e, 0x35, 0x2e, 0x31, 0x4, 0x5, 0x30, 0x3, 0x81, 0x1, 0x6})}, wantErr: false},
-		{name: "passwordTooYoung", args: args{packet: decodePacket([]byte{0xa0, 0x24, 0x30, 0x22, 0x4, 0x19, 0x31, 0x2e, 0x33, 0x2e, 0x36, 0x2e, 0x31, 0x2e, 0x34, 0x2e, 0x31, 0x2e, 0x34, 0x32, 0x2e, 0x32, 0x2e, 0x32, 0x37, 0x2e, 0x38, 0x2e, 0x35, 0x2e, 0x31, 0x4, 0x5, 0x30, 0x3, 0x81, 0x1, 0x7})}, wantErr: false},
-		{name: "passwordInHistory", args: args{packet: decodePacket([]byte{0xa0, 0x24, 0x30, 0x22, 0x4, 0x19, 0x31, 0x2e, 0x33, 0x2e, 0x36, 0x2e, 0x31, 0x2e, 0x34, 0x2e, 0x31, 0x2e, 0x34, 0x32, 0x2e, 0x32, 0x2e, 0x32, 0x37, 0x2e, 0x38, 0x2e, 0x35, 0x2e, 0x31, 0x4, 0x5, 0x30, 0x3, 0x81, 0x1, 0x8})}, wantErr: false},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			if err := AddDescriptions(test.args.packet); (err != nil) != test.wantErr {
-				t.Errorf("addControlDescriptions() error = %v, wantErr %v", err, test.wantErr)
-			}
-		})
 	}
 }
 
